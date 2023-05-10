b99de9317c527798e0e4c597515a0dd6afaae979
alenabaska <akunets@akunetss-MacBook-Air.local>
2023-05-10 14:37:48 -0600

    create login_page

---
+++ pages/locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
--- test_main_page.py
+++ test_main_page.py
@@ -8,13 +8,6 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
