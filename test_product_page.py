7a76cc2b045713f2f191dfe34a15ec9660498edb
alenabaska <132595359+alenabaska@users.noreply.github.com>
2023-05-20 14:33:58 -0400

    add tests for product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,13 @@
+import random
 import pytest
+
+from .pages.locators import ProductPageLocators
+from .pages.basket_page import BasketPage
+from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)  
@@ -29,6 +36,23 @@ def test_guest_can_add_product_to_basket_with_params(browser, link):
     page.should_be_price_the_same()
     page.should_be_name_the_same() 
 
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    basket_page = BasketPage(browser, link)
+    basket_page.open()
+    basket_page.open_basket()
+    basket_page.is_basket_empty()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
+
+@pytest.mark.need_review
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)  
@@ -48,3 +72,30 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()                         
     page.add_to_busket()
     page.should_disappeared()
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        chislo = random.randint(0, 1000) #генерация случайного числа для логина и пароля пользователя
+        email = "email"+str(chislo*chislo) + "@mail" + ".com" #генерация логина
+        password = "qwerty"+str(chislo*chislo) #генерация пароля
+        page_login = LoginPage(browser, link)
+        page_login.open()
+        page_login.register_new_user(email, password)
+        page_base = BasePage(browser, link)
+        page_base.should_be_authorized_user()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        item_page = ProductPage(browser, link)
+        item_page.open()
+        item_page.add_to_basket()
+        item_page.check_item_name()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        item_page = ProductPage(browser, link)
+        item_page.open()
+        item_page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
